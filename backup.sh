1bb7cae0c94a6761b6001b44dc0638d8c558bcd9
marcosmartinezfco <marcosmartinezfco@gmail.com>
2021-11-21 13:52:34 +0100

    Add basic logging functionality
    Signed-off-by: marcosmartinezfco <marcosmartinezfco@gmail.com>

--- backup.sh
+++ backup.sh
@@ -18,7 +18,6 @@ function header() {
   spacer
 }
 
-
 function menu() {
   echo Menu
   echo -e "\t1) Perform a backup"
@@ -36,7 +35,7 @@ function menu() {
     if [ $option -lt 1 -o $option -gt 4 ]; then
         return $option
     else
-      return 5
+      return 5 #This way the default option in main will be executed
     fi
   fi
 }
@@ -46,39 +45,35 @@ function backup() {
   echo -e "Backup tool\n"
   read -p "Path of the directory >> " path
 
-  if [ -d "$path" -o -f "$path" ]; then
+  if [ -d "$path" ]; then
     echo -e "\nWe will do a backup of the directory $path"
     read -p "Do yo want to proceed (y/n)? >> " option
     if [ $option = "y" -o $option = "Y" -o $option = "yes" -o $option = "Yes" -o $option = "YES" ]; then
       name="$(basename "$path")-$(date +%Y%m%d-%H%M)"
       tar -czf "$workdir/$name.tar.gz" "$path"
-      echo -e "\nThe file $path has been backed up"
+      echo -e "\nThe directory $path has been backed up"
+      echo -e "$(date +%Y%m%d-%H%M)\tINFO\tThe directory $path has been backed up" >> "$workdir/backup.log"
       echo -e "Taking you to the main menu"
-      sleep 2
-      clear
-      main
     elif [ $option = "n" -o $option = "N" -o $option = "No" -o $option = "NO" -o $option = "no" ]; then
-      clear
-      main
+      echo "Okay, you'll be redirected to the main menu"
     else
       echo "Invalid option, you'll be redirected to the main menu"
-      sleep 2
-      clear
-      main
+      echo -e "$(date +%Y%m%d-%H%M)\tERROR\tInvalid option in the backup tool menu" >> "$workdir/backup.log"
     fi
   else
     echo "The specified path doesn't exit, taking you to the main menu"
-    sleep 2
-    clear
-    main
+    echo -e "$(date +%Y%m%d-%H%M)\tERROR\tInvalid path specified in the backup tool menu" >> "$workdir/backup.log"
   fi
+  sleep 2
+  clear
+  main
 }
 
 function cronBackup() {
   clear
   echo -e "Program backups tool\n"
-  read -p "Absolute path to the file >> " path
-  if [ -d "$path" -o -f "$path" ]; then
+  read -p "Absolute path to the directory >> " path
+  if [ -d "$path" ]; then
     read -p "Hour of the backup (0:00 - 23:59) >> " time
     read -p "The backup will execute at $time. Do you agree? (y/n) >> " option
     if [ $option = "y" -o $option = "Y" -o $option = "yes" -o $option = "Yes" -o $option = "YES" ]; then
@@ -88,27 +83,22 @@ function cronBackup() {
         crontab -l > /tmp/crontabaso
         echo "$min $hour * * 1-7 tar -czf $workdir/$(basename "$path")-$(date +%Y%m%d-%H%M).tar.gz $path" >> /tmp/crontabaso
         crontab /tmp/crontabaso
-        sleep 2
-        clear
-        main
+        echo "Task added to crontab file, taking you to the main menu"
+        echo -e "$(date +%Y%m%d-%H%M)\tINFO\tTask added to crontab file" >> "$workdir/backup.log"
       else
-        echo -e "\nInvalid hour format, you'll be redirected to the main menu"
-        sleep 2
-        clear
-        main
+        echo "Invalid hour format, you'll be redirected to the main menu"
+        echo -e "$(date +%Y%m%d-%H%M)\tERROR\tInvalid hour format specified in the cron backup tool" >> "$workdir/backup.log"
       fi
     else
       echo -e "\nOkay, you'll be redirected to the main menu"
-      sleep 2
-      clear
-      main
     fi
   else
     echo -e "\nInvalid path, you'll be redirected to the main menu"
-    sleep 2
-    clear
-    main
+    echo -e "$(date +%Y%m%d-%H%M)\tERROR\tInvalid path selected in the cron backup tool" >> "$workdir/backup.log"
   fi
+  sleep 2
+  clear
+  main
 }
 
 function restoreBackup() {
@@ -124,9 +114,11 @@ function restoreBackup() {
   read -p "Which one do you want to recover >> " answer
   if [ -f "$workdir/$answer" ]; then
     echo "Restoring $answer ..."
+    echo -e "$(date +%Y%m%d-%H%M)\tINFO\tRestoring backup file $answer" >> "$workdir/backup.log"
     tar -xf "$workdir/$answer"
   else
     echo "There is no such file, taking you to the main menu"
+    echo -e "$(date +%Y%m%d-%H%M)\tERROR\tInvalid bakup file selected in the restore backup tool" >> "$workdir/backup.log"
   fi
   sleep 2
   clear
